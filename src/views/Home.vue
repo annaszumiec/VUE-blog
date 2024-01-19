e0bf18dcdbea6c6a1b2083ca28eb4cc889add73a
annamaria <szumiecam@gmail.com>
2024-01-19 19:56:02 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+
+    <div v-else>
+      <Spinner />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import getPosts from "../composables/getPosts";
+// component imports
+import PostList from "../components/PostList.vue";
+import Spinner from "../components/Spinner.vue";
+
+export default {
+  name: "Home",
+  components: { PostList, Spinner },
+  setup() {
+    const { posts, error, load } = getPosts();
+    load();
+
+    return { posts, error };
+  },
+};
+</script>
