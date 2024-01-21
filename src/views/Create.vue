8bc918ebf0c475525bf82049424bc8ba2fa6db8f
annamaria <szumiecam@gmail.com>
2024-01-21 14:40:42 +0100

    adding post

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -16,6 +16,7 @@
 <script>
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import { projectFirestore } from "@/firebase/config";
 
 export default {
   setup() {
@@ -40,12 +41,10 @@ export default {
         body: body.value,
         tags: tags.value,
       };
-      await fetch("http://localhost:3000/posts", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(post),
-      });
+      const restore = await projectFirestore.collection("posts").add(post);
+      console.log(restore);
 
+      //redirect to homepage
       router.push({ name: "Home" });
     };
 
